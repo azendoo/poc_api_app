f24cd5458f81078a13517c594e2b3591fc93613d
Simon Bonnard <smn.bonnard@gmail.com>
2013-07-02 14:36:36 +0200

    routes polishing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 PocApiApp::Application.routes.draw do
-  devise_for :users, :controllers => {registrations: 'registrations'}, defaults: { format: 'json' }
-  resources :users
+
+  devise_for :users, controllers: { registrations: 'registrations'},
+                                    skip: [:sessions, :registrations, :password, :confirmation],
+                                    defaults: { format: 'json' }
+
+  devise_scope :user do
+    post 'users', to: 'registrations#create'
+    get 'users', to: 'users#index'
+    get '/users/:id', to: 'users#show', as: 'user'
+    put '/users/:id', to: 'users#update'
+    delete '/users/:id', to:'users#destroy'
+  end
+
   resources :tasks, except: :edit
-  resources :tokens, only: [:create, :destroy, :show], :defaults => { :format => 'json' }
-  match 'tokens', :via => :options, :action => :create, :controller => 'tokens', defaults: { format: 'json' }
+  resources :tokens, only: [:create, :destroy, :show], defaults: { format: 'json' }
+
+  match 'tokens', via: :options, action: :create, controller: 'tokens', defaults: { format: 'json' }
+
 end
