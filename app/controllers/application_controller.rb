1f9a2c7daf86dc6cde5566684419312e00e789c2
Simon Bonnard <smn.bonnard@gmail.com>
2013-07-03 18:03:24 +0200

    standard handling of mime type requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::API
   include ActionController::MimeResponds
+  before_filter :ensure_json_request
+
+  private
+  def ensure_json_request
+    render json: '{}', status: 406 unless [Mime::ALL,Mime::JSON].include? request.format
+  end
 end
