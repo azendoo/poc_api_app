47b9f0b1b3de7f0e999da153965ff55150fb6446
Simon Bonnard <smn.bonnard@gmail.com>
2013-07-08 17:47:45 +0200

    home#index now reflects current routes, login/logout were added.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,12 @@
 class HomeController < ApplicationController
 
+  # XXX : temporary
   def index
     links = {
-      users_url: users_url + "/{id}",
-      tasks_url: tasks_url + "/{id}",
-      tokens: tokens_url + "/{id}"
+      login_url:  url_for(controller:'tokens', action: 'create'),
+      logout_url: url_for(controller:'tokens', action: 'destroy'),
+      users_url:  users_url   + "/{id}",
+      tasks_url:  tasks_url   + "/{id}"
     }
     render json: links.to_json
   end
