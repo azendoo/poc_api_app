7e934d4db2d162c18716e94dc38b01847ee02427
Simon Bonnard <smn.bonnard@gmail.com>
2013-07-12 16:30:07 +0200

    custom devise failureapp

---
+++ config/initializers/custom_auth_failure.rb
@@ -0,0 +1,25 @@
+class CustomFailure < Devise::FailureApp
+  def respond
+    http_auth
+  end
+
+  def redirect
+    if flash[:timedout] && flash[:alert]
+      flash.keep(:timedout)
+      flash.keep(:alert)
+    else
+      flash[:alert] = i18n_message
+    end
+  end
+
+  def http_auth
+    self.status = 401
+    self.content_type = Mime::JSON
+    self.response_body = http_auth_body
+  end
+
+  def http_auth_body
+    return i18n_message unless request_format
+    { :errors => i18n_message }.to_json.encode("UTF-8")
+  end
+end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -211,7 +211,7 @@ Devise.setup do |config|
   # should add them to the navigational formats lists.
   #
   # The "*/*" below is required to match Internet Explorer requests.
-  # config.navigational_formats = ["*/*", :html]
+  config.navigational_formats = [:json]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
@@ -229,7 +229,9 @@ Devise.setup do |config|
   #   manager.intercept_401 = false
   #   manager.default_strategies(:scope => :user).unshift :some_external_strategy
   # end
-
+  config.warden do |manager|
+    manager.failure_app = CustomFailure
+  end
 
   # ==> Mountable engine configurations
   # When using Devise inside an engine, let's call it `MyEngine`, and this engine
