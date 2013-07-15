4e59b61bd15dc427cfaaa45e25d506a9c6ba668f
Simon Bonnard <smn.bonnard@gmail.com>
2013-07-15 11:18:22 +0200

    removed non-existing routes from default routing spec

--- spec/routing/tasks_routing_spec.rb
+++ spec/routing/tasks_routing_spec.rb
@@ -7,16 +7,8 @@ describe TasksController do
       get("/tasks").should route_to("tasks#index")
     end
 
-    it "routes to #new" do
-      get("/tasks/new").should route_to("tasks#new")
-    end
-
     it "routes to #show" do
       get("/tasks/1").should route_to("tasks#show", :id => "1")
-    end
-
-    it "routes to #edit" do
-      get("/tasks/1/edit").should route_to("tasks#edit", :id => "1")
     end
 
     it "routes to #create" do
