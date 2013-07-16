13becafcb4f0fec273800a2c4350d5cb3b50efe1
Simon Bonnard <smn.bonnard@gmail.com>
2013-07-16 11:33:38 +0200

    added a basic test of registration

---
+++ spec/controllers/registration_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe RegistrationsController do
+  include Devise::TestHelpers
+
+  before(:each) do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    @request.env["HTTP_ACCEPT"] = Mime::JSON
+  end
+
+  describe "POST create" do
+
+    it "should succeed with valid credentials" do
+      post :create, :user => { email: "foo@bar.ok", password: "please", password_confirmation: "please" }
+      response.should be_success
+      JSON.parse(response.body).keys.include?("auth_token")
+    end
+
+    it "should fail with invalid credentials" do
+      post :create, :user => {}
+      response.status.should eq(422)
+    end
+
+  end
+
+end
