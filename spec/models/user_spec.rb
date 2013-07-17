0b5a3f459ef1e60ef4d5f70e2520b6ef95971d49
Simon Bonnard <smn.bonnard@gmail.com>
2013-07-17 17:35:49 +0200

    standard user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,33 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @attr = {
+      :email => 'foobar@domain.com',
+      :password => 'please',
+      :password_confirmation => 'please'
+    }
+  end
+
+  it "should create a new instance given valid attributes" do
+    User.create!(@attr)
+  end
+
+  it "should require an email address" do
+    no_email_user = User.new(@attr.merge(:email => ''))
+    no_email_user.should_not be_valid
+  end
+
+  it "should reject duplicate email address" do
+    User.create!(@attr)
+    user_with_duplicate_email = User.new(@attr)
+    user_with_duplicate_email.should_not be_valid
+  end
+
+  it "should create an authentication token" do
+    user = User.create!(@attr)
+    user.has_attribute? :authentication_token
+  end
+
 end
