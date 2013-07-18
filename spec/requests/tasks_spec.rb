9752862792455b345020bc732b41d6f1115db3f3
Simon Bonnard <smn.bonnard@gmail.com>
2013-07-18 16:49:21 +0200

    spec request for tasks

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -1,11 +1,29 @@
-#require 'spec_helper'
-#
-#describe "Tasks" do
-#  describe "GET /tasks" do
-#    it "works! (now write some real specs)" do
-#      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-#      get tasks_path
-#      response.status.should be(200)
-#    end
-#  end
-#end
+require 'spec_helper'
+
+describe "Tasks" do
+
+  env_headers = {'HTTP_ACCEPT' => Mime::JSON, "devise.mapping" => Devise.mappings[:user] }
+
+  describe "GET /tasks" do
+    context "with valid credentials" do
+      user = FactoryGirl.build(:user)
+      authorization_header =  ActionController::HttpAuthentication::Basic.encode_credentials(user.authentication_token, nil)
+      env_headers['HTTP_AUTHORIZATION'] = authorization_header
+
+      it "should succeed" do
+        get '/tasks', nil, env_headers
+      end
+    end
+
+    context "with invalid credentials" do
+      authorization_header = ActionController::HttpAuthentication::Basic.encode_credentials("123456", nil)
+      env_headers['HTTP_AUTHORIZATION'] = authorization_header
+
+      it "should fail" do
+        get '/tasks', nil, env_headers
+        response.status.should eq(401)
+      end
+    end
+
+  end
+end
