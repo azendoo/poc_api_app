cb28df3faf77064660400dbfd317fbc8f3aae259
Simon Bonnard <smn.bonnard@gmail.com>
2013-07-19 10:44:22 +0200

    fixed a stupid scope mistake

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -3,14 +3,15 @@ require 'spec_helper'
 describe "Tasks" do
 
   env_headers = {'HTTP_ACCEPT' => Mime::JSON, "devise.mapping" => Devise.mappings[:user] }
+  let(:user) { FactoryGirl.build(:user) }
 
   describe "GET /tasks" do
     context "with valid credentials" do
-      user = FactoryGirl.build(:user)
-      authorization_header =  ActionController::HttpAuthentication::Basic.encode_credentials(user.authentication_token, nil)
-      env_headers['HTTP_AUTHORIZATION'] = authorization_header
 
       it "should succeed" do
+        authorization_header =  ActionController::HttpAuthentication::Basic.encode_credentials(user.authentication_token, nil)
+        env_headers['HTTP_AUTHORIZATION'] = authorization_header
+
         get '/tasks', nil, env_headers
       end
     end
