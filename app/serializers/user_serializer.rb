4a2714bed8f3aab8d5295c0d4764a9f6d8f552f4
Simon Bonnard <smn.bonnard@gmail.com>
2013-07-23 16:36:14 +0200

    update user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
+  embed :ids, include: true
   attributes :id, :email, :url
+
+  has_many :tasks, embed: :ids, include: false
 
   def url
     user_url(object)
