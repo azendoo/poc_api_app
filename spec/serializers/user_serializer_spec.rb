1afb5eda95766319e32db1e3757a19d3d9892e0c
Simon Bonnard <smn.bonnard@gmail.com>
2013-07-23 16:38:13 +0200

    basic user serializer spec

---
+++ spec/serializers/user_serializer_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe UserSerializer do
+  let(:attributes) { FactoryGirl.attributes_for(resource_name) }
+
+  it { should have_attribute(:id) }
+  it { should have_attribute(:email) }
+  it { should have_attribute(:url) }
+
+  it { should embed(:ids) }
+
+  it { should include_root }
+
+end
