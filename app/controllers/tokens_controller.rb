ecb5e79ca400c9b73015af4bb0d78bcaa7705285
Simon Bonnard <smn.bonnard@gmail.com>
2013-07-25 17:51:01 +0200

    ugly quick fix (refactor asap)

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,7 +1,7 @@
 class TokensController < Devise::SessionsController
   skip_before_filter :authenticate_user!
   skip_before_filter :verify_authenticity_token
-  skip_before_filter :ensure_tokens_presence, :only => [:create]
+  skip_before_filter :ensure_tokens_presence
   skip_after_filter :update_last_activity, :only => [:create]
 
   respond_to :json
@@ -27,13 +27,21 @@ class TokensController < Devise::SessionsController
 
   end
 
-  # XXX : currently reset authentication token, maybe we should
-  # directly return the new token.
   def destroy
     response.headers.merge!({
       'Pragma'        => 'no-cache',
       'Cache-Control' => 'no-store',
     })
+
+    # XXX ; ugly, change ASAP
+    if params[:auth_token].present?
+      current_token = params[:auth_token]
+    elsif request.authorization && request.authorization =~ /^Basic (.*)/m
+      current_token = Base64.decode64($1).split(/:/, 2)
+      current_token = current_token.first
+    end
+
+    @user = User.where(authentication_token: current_token).first
 
     warden.authenticate!(:scope => resource_name, :store => false)
     current_user.reset_authentication_token!
