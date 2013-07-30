f8160f34728404d88c70ff00a5256631bedc690a
Simon Bonnard <smn.bonnard@gmail.com>
2013-07-30 12:07:36 +0200

    add a check for time-out

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -2,7 +2,6 @@ class TokensController < Devise::SessionsController
   skip_before_filter :authenticate_user!
   skip_before_filter :verify_authenticity_token
   skip_before_filter :ensure_tokens_presence, :only => [:create]
-  skip_after_filter :update_last_activity, :only => [:create]
 
   respond_to :json
 
@@ -18,9 +17,15 @@ class TokensController < Devise::SessionsController
       params["user"] ||= {}
       params["user"].merge!(email: params[:email], password: params[:password])
 
+      # in case token timed out, we should reset it
+      if timedout?
+        current_user.reset_authentication_token
+      end
+
       warden.authenticate!(:scope => resource_name, :store => false)
 
       render json: { auth_token: current_user.authentication_token }
+
     else
       render json: { errors: "Missing email or password attribute" }, status: :unauthorized
     end
