1afe35995fd3e1af20a5b7a179e7de363f4a808c
Simon Bonnard <smn.bonnard@gmail.com>
2013-07-30 12:08:47 +0200

    update of registration spec

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -2,21 +2,44 @@ require 'spec_helper'
 
 describe RegistrationsController do
 
+  let(:user){ FactoryGirl.build(:user)}
+
+  # Authentication attributes :
+  def valid_attributes
+    {:email => user.email, :password => user.password}
+  end
+
+  def invalid_attributes
+    {:email => 'foobardomaincom', :password => ''}
+  end
+
   before do
     @request.env["devise.mapping"] = Devise.mappings[:user]
     @request.env["HTTP_ACCEPT"] = Mime::JSON
+    @request.env["CONTENT_TYPE"] = Mime::JSON
   end
 
   describe "#create" do
-
     it "should succeed with valid credentials" do
-      post :create, { email: "foo@bar.ok", password: "please" }
+      # Required to send raw JSON data :
+      request.env['RAW_POST_DATA'] = valid_attributes.to_json
+
+      # Create a new account :
+      post :create, valid_attributes
+
+      # Checks :
       response.should be_success
       JSON.parse(response.body).keys.include?("auth_token")
     end
 
     it "should fail with invalid credentials" do
-      post :create, { email: "", password: ""}
+      # Required to send raw JSON data :
+      request.env['RAW_POST_DATA'] = invalid_attributes.to_json
+
+      # Create a new account :
+      post :create, invalid_attributes
+
+      # Checks :
       response.status.should eq(400)
     end
 
