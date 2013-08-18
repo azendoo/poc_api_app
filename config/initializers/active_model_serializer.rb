03f013ebf487b75edec200562ac0ba3a4fbb4f51
Simon Bonnard <smn.bonnard@gmail.com>
2013-08-18 13:06:11 +0200

    correctly handle mongoid criteria in AMS

--- config/initializers/active_model_serializer.rb
+++ config/initializers/active_model_serializer.rb
@@ -1,8 +1,10 @@
 ActiveSupport.on_load(:active_model_serializers) do
   # Disable for all serializers (except ArraySerializer)
   ActiveModel::Serializer.root = false
-
   # Disable for ArraySerializer
   ActiveModel::ArraySerializer.root = false
 end
 
+# Support for Mongoid :
+Mongoid::Document.send(:include, ActiveModel::SerializerSupport)
+Mongoid::Criteria.delegate(:active_model_serializer, to: :to_a)
