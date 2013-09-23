f3a39af615d004e947301cfe07cfe71a39b80787
Simon Bonnard <smn.bonnard@gmail.com>
2013-09-23 12:08:52 +0200

    add a seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Rake::Task["db:drop"].invoke
+
+user1 = User.create(email: "tbishop@total.com", password: "please")
+puts "User #{user1.email} account created."
+
+user2 = User.create(email: "robin@azendoo.com", password: "please")
+puts "User #{user2.email} account created."
+
+task  = user1.tasks.new(label: "This is a task")
+task.save!
+puts "New task created : #{task.label}"
+
+task  = user1.tasks.new(label: "Another task")
+task.save!
+puts "New task created : #{task.label}"
+
+task  = user2.tasks.new(label: "Awesome task")
+task.save!
+puts "New task created : #{task.label}"
+
