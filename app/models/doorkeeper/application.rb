23ed5e1ce6c1902c8bdeda472b3f94b1275dac8f
Simon Bonnard <smn.bonnard@gmail.com>
2013-09-30 17:45:23 +0200

    set up doorkeeper models on API side

---
+++ app/models/doorkeeper/application.rb
@@ -0,0 +1,15 @@
+module Doorkeeper
+  class Application
+    include Mongoid::Document
+    include Mongoid::Timestamps
+
+    self.store_in collection: :oauth_applications
+
+    field :name, :type => String
+    field :uid, :type => String
+    field :secret, :type => String
+    field :redirect_uri, :type => String
+
+    has_many :authorized_tokens, :class_name => "Doorkeeper::AccessToken"
+  end
+end
