8e12652d0cc958dcb9c38a9235f2564224e28cee
Simon Bonnard <smn.bonnard@gmail.com>
2013-10-18 12:01:03 +0200

    [WIP] Release an OAuth call to generate a token on user registration

--- app/controllers/v1/registrations_controller.rb
+++ app/controllers/v1/registrations_controller.rb
@@ -17,9 +17,24 @@ class V1::RegistrationsController < Devise::RegistrationsController
   def create
     u = User.new(resource_params)
 
-    if u.save!
-      render json: { email: u.email, auth_token: u.authentication_token },
-        status: :created
+
+    #XXX WIP
+    if u.save
+      # ask for a brand new oauth token
+      if u.access_tokens.blank?
+        response  = RestClient.post("#{APP_CONFIG['doorkeeper_app_url']}/oauth/token", \
+                                    grant_type: "password", \
+                                    client_id: APP_CONFIG['doorkeeper_app_id'], \
+                                    client_secret: APP_CONFIG['doorkeeper_app_secret'], \
+                                    username: params['email'], \
+                                    password: params['password'])
+
+        token_infos = JSON.parse(response.body)
+        render json: token_infos, status: :created
+      else
+        # lets do something in the future if there's a problem
+        # with the oauth provider
+      end
     else
       clean_up_passwords u
       render  json: { errors: u.errors }, status: :unprocessable_entity
